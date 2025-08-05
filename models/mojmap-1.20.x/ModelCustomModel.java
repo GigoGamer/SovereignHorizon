e6dca37959ba1cf6b9456f46184d99a98d08fa4b
Goshiggy <Shigg@404PageNotFound>
2025-08-05 14:11:43 -0500

    added rived ghast model+texture and catechism texture

---
+++ models/mojmap-1.20.x/ModelCustomModel.java
@@ -0,0 +1,107 @@
+// Made with Blockbench 4.12.5
+// Exported for Minecraft version 1.17 or later with Mojang mappings
+// Paste this class into your mod and generate all required imports
+
+public class ModelCustomModel<T extends Entity> extends EntityModel<T> {
+	// This layer location should be baked with EntityRendererProvider.Context in
+	// the entity renderer and passed into this model's constructor
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(
+			new ResourceLocation("modid", "custommodel"), "main");
+	private final ModelPart body;
+	private final ModelPart leftbody;
+	private final ModelPart righthalf;
+	private final ModelPart tentacles_0;
+	private final ModelPart tentacles_1;
+	private final ModelPart tentacles_2;
+	private final ModelPart tentacles_3;
+	private final ModelPart tentacles_4;
+	private final ModelPart tentacles_5;
+	private final ModelPart tentacles_6;
+	private final ModelPart tentacles_7;
+
+	public ModelCustomModel(ModelPart root) {
+		this.body = root.getChild("body");
+		this.leftbody = this.body.getChild("leftbody");
+		this.righthalf = this.body.getChild("righthalf");
+		this.tentacles_0 = this.body.getChild("tentacles_0");
+		this.tentacles_1 = this.body.getChild("tentacles_1");
+		this.tentacles_2 = this.body.getChild("tentacles_2");
+		this.tentacles_3 = this.body.getChild("tentacles_3");
+		this.tentacles_4 = this.body.getChild("tentacles_4");
+		this.tentacles_5 = this.body.getChild("tentacles_5");
+		this.tentacles_6 = this.body.getChild("tentacles_6");
+		this.tentacles_7 = this.body.getChild("tentacles_7");
+	}
+
+	public static LayerDefinition createBodyLayer() {
+		MeshDefinition meshdefinition = new MeshDefinition();
+		PartDefinition partdefinition = meshdefinition.getRoot();
+
+		PartDefinition body = partdefinition.addOrReplaceChild("body",
+				CubeListBuilder.create().texOffs(0, 0)
+						.addBox(1.0F, 5.0F, -1.0F, 3.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)).texOffs(0, 0)
+						.addBox(-3.0F, -5.0F, -2.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(0.0F, 16.0F, 0.0F));
+
+		PartDefinition leftbody = body.addOrReplaceChild("leftbody",
+				CubeListBuilder.create().texOffs(0, 0)
+						.addBox(-8.0F, -16.0F, -8.0F, 5.0F, 16.0F, 16.0F, new CubeDeformation(0.0F)).texOffs(42, 0)
+						.addBox(-3.0F, -11.0F, -8.0F, 2.0F, 11.0F, 13.0F, new CubeDeformation(0.0F)).texOffs(0, 64)
+						.addBox(-1.0F, -6.0F, -8.0F, 2.0F, 6.0F, 11.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(0.0F, 8.0F, 0.0F));
+
+		PartDefinition righthalf = body.addOrReplaceChild("righthalf",
+				CubeListBuilder.create().texOffs(0, 32)
+						.addBox(4.0F, -16.0F, -8.0F, 4.0F, 16.0F, 16.0F, new CubeDeformation(0.0F)).texOffs(40, 32)
+						.addBox(2.0F, -16.0F, -6.0F, 2.0F, 12.0F, 14.0F, new CubeDeformation(0.0F)).texOffs(40, 58)
+						.addBox(0.0F, -16.0F, -4.0F, 2.0F, 9.0F, 12.0F, new CubeDeformation(0.0F)).texOffs(68, 58)
+						.addBox(-1.0F, -13.0F, -3.0F, 1.0F, 6.0F, 10.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(0.0F, 8.0F, 0.0F));
+
+		PartDefinition tentacles_0 = body.addOrReplaceChild("tentacles_0", CubeListBuilder.create().texOffs(72, 27)
+				.addBox(-1.0F, 0.0F, -1.0F, 2.0F, 9.0F, 2.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(-3.8F, 7.0F, -5.0F));
+
+		PartDefinition tentacles_1 = body.addOrReplaceChild("tentacles_1", CubeListBuilder.create().texOffs(72, 14)
+				.addBox(-1.0F, 0.0F, -1.0F, 2.0F, 11.0F, 2.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(0.3F, 7.0F, -5.0F));
+
+		PartDefinition tentacles_2 = body.addOrReplaceChild("tentacles_2", CubeListBuilder.create().texOffs(72, 38)
+				.addBox(-1.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(6.3F, 7.0F, -5.0F));
+
+		PartDefinition tentacles_3 = body.addOrReplaceChild("tentacles_3", CubeListBuilder.create().texOffs(72, 27)
+				.addBox(-1.0F, 0.0F, -1.0F, 2.0F, 9.0F, 2.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(-6.3F, 7.0F, 0.0F));
+
+		PartDefinition tentacles_4 = body.addOrReplaceChild("tentacles_4", CubeListBuilder.create().texOffs(26, 64)
+				.addBox(-1.0F, 0.0F, -1.0F, 2.0F, 13.0F, 2.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(-1.3F, 7.0F, 0.0F));
+
+		PartDefinition tentacles_5 = body.addOrReplaceChild("tentacles_5", CubeListBuilder.create().texOffs(72, 14)
+				.addBox(-1.0F, 0.0F, -1.0F, 2.0F, 11.0F, 2.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(4.8F, 7.0F, 0.0F));
+
+		PartDefinition tentacles_6 = body.addOrReplaceChild("tentacles_6", CubeListBuilder.create().texOffs(72, 0)
+				.addBox(-1.0F, 0.0F, -1.0F, 2.0F, 12.0F, 2.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(-3.8F, 7.0F, 5.0F));
+
+		PartDefinition tentacles_7 = body.addOrReplaceChild("tentacles_7", CubeListBuilder.create().texOffs(26, 64)
+				.addBox(-1.0F, 0.0F, -1.0F, 2.0F, 13.0F, 2.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(6.3F, 7.0F, 5.0F));
+
+		return LayerDefinition.create(meshdefinition, 128, 128);
+	}
+
+	@Override
+	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay,
+			float red, float green, float blue, float alpha) {
+		body.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+	}
+
+	public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw,
+			float headPitch) {
+		this.body.yRot = netHeadYaw / (180F / (float) Math.PI);
+		this.body.xRot = headPitch / (180F / (float) Math.PI);
+	}
+}
