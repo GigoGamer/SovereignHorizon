bf33d6a7c38f60c0faeda7d9cfb151a17ba04ba1
Goshiggy <Shigg@404PageNotFound>
2025-08-07 17:32:12 -0500

    bugfixing

--- src/main/java/net/mcreator/sovereignhorizon/init/SovereignHorizonModItems.java
+++ src/main/java/net/mcreator/sovereignhorizon/init/SovereignHorizonModItems.java
@@ -13,7 +13,6 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.BlockItem;
 
-import net.mcreator.sovereignhorizon.item.SoverignHorizonItem;
 import net.mcreator.sovereignhorizon.item.ElixiteArmorItem;
 import net.mcreator.sovereignhorizon.item.ElixisUpgradeSmithingTemplateItem;
 import net.mcreator.sovereignhorizon.item.ElixisShardItem;
@@ -23,7 +22,6 @@ import net.mcreator.sovereignhorizon.SovereignHorizonMod;
 public class SovereignHorizonModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, SovereignHorizonMod.MODID);
 	public static final RegistryObject<Item> TEST = block(SovereignHorizonModBlocks.TEST);
-	public static final RegistryObject<Item> SOVERIGN_HORIZON = REGISTRY.register("soverign_horizon", () -> new SoverignHorizonItem());
 	public static final RegistryObject<Item> CATECHISM_SPAWN_EGG = REGISTRY.register("catechism_spawn_egg", () -> new ForgeSpawnEggItem(SovereignHorizonModEntities.CATECHISM, -10092442, -65281, new Item.Properties()));
 	public static final RegistryObject<Item> SODALITE = block(SovereignHorizonModBlocks.SODALITE);
 	public static final RegistryObject<Item> SODALITE_STAIRS = block(SovereignHorizonModBlocks.SODALITE_STAIRS);
