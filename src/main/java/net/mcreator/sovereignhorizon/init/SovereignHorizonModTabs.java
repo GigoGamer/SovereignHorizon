37dd7813d75b880b9c4cb4e2cc9f5c791f4376a4
Goshiggy <Shigg@404PageNotFound>
2025-08-08 10:49:39 -0500

    Creative Tab bugfix!!! VERY IMPORTANT

---
+++ src/main/java/net/mcreator/sovereignhorizon/init/SovereignHorizonModTabs.java
@@ -0,0 +1,59 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.sovereignhorizon.init;
+
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.network.chat.Component;
+import net.minecraft.core.registries.Registries;
+
+import net.mcreator.sovereignhorizon.SovereignHorizonMod;
+
+public class SovereignHorizonModTabs {
+	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, SovereignHorizonMod.MODID);
+	public static final RegistryObject<CreativeModeTab> SOVEREIGN_HORIZON = REGISTRY.register("sovereign_horizon",
+			() -> CreativeModeTab.builder().title(Component.translatable("item_group.sovereign_horizon.sovereign_horizon")).icon(() -> new ItemStack(SovereignHorizonModBlocks.SODALITE.get())).displayItems((parameters, tabData) -> {
+				tabData.accept(SovereignHorizonModBlocks.TEST.get().asItem());
+				tabData.accept(SovereignHorizonModItems.CATECHISM_SPAWN_EGG.get());
+				tabData.accept(SovereignHorizonModBlocks.SODALITE.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.SODALITE_STAIRS.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.SODALITE_SLAB.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.SODALITE_WALL.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.SODALITE_PRESSURE_PLATE.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.SODALITE_BUTTON.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.SPARSE_DEEP_LICHEN_COVERED_SODALITE.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.DEEP_LICHEN_COVERED_SODALITE.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.COBBLED_SODALITE.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.COBBLED_SODALITE_STAIRS.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.COBBLED_SODALITE_SLAB.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.COBBLED_SODALITE_WALL.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.POLISHED_SODALITE.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.POLISHED_SODALITE_STAIRS.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.POLISHED_SODALITE_SLAB.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.POLISHED_SODALITE_WALL.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.SODALITE_BRICKS.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.SODALITE_BRICK_STAIRS.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.SODALITE_BRICK_SLAB.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.SODALITE_BRICK_WALL.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.CRACKED_SODALITE_BRICKS.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.SODALITE_TILES.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.SODALITE_TILE_STAIRS.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.SODALITE_TILE_SLAB.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.SODALITE_TILE_WALL.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.CRACKED_SODALITE_TILES.get().asItem());
+				tabData.accept(SovereignHorizonModItems.ELIXIS_SHARD.get());
+				tabData.accept(SovereignHorizonModItems.ELIXIS_UPGRADE_SMITHING_TEMPLATE.get());
+				tabData.accept(SovereignHorizonModItems.ELIXITE_ARMOR_HELMET.get());
+				tabData.accept(SovereignHorizonModItems.ELIXITE_ARMOR_CHESTPLATE.get());
+				tabData.accept(SovereignHorizonModItems.ELIXITE_ARMOR_LEGGINGS.get());
+				tabData.accept(SovereignHorizonModItems.ELIXITE_ARMOR_BOOTS.get());
+				tabData.accept(SovereignHorizonModItems.CATECHISM_WING.get());
+				tabData.accept(SovereignHorizonModBlocks.INFUSION_TABLE.get().asItem());
+				tabData.accept(SovereignHorizonModBlocks.LARIMAR.get().asItem());
+			}).build());
+}
