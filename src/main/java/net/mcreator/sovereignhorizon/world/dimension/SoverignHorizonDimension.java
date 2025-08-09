f150dddc0d37f7f10cb0656af62d94af7ef8267e
GigoGamer <maxprantil@mac.lan>
2025-08-09 08:49:22 -0700

    changed a few bits of code

--- src/main/java/net/mcreator/sovereignhorizon/world/dimension/SoverignHorizonDimension.java
+++ src/main/java/net/mcreator/sovereignhorizon/world/dimension/SoverignHorizonDimension.java
@@ -1,6 +1,24 @@
 
 package net.mcreator.sovereignhorizon.world.dimension;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.client.event.RegisterDimensionSpecialEffectsEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.client.renderer.DimensionSpecialEffects;
+
+import net.mcreator.sovereignhorizon.procedures.SoverignHorizonPlayerLeavesDimensionProcedure;
+import net.mcreator.sovereignhorizon.procedures.SoverignHorizonPlayerEntersDimensionProcedure;
+
 @Mod.EventBusSubscriber
 public class SoverignHorizonDimension {
 	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
