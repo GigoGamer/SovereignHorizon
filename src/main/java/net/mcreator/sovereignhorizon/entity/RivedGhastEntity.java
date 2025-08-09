afe370a27e5cf0d8199f44321ffd7480ffd9a2e1
Goshiggy <Shigg@404PageNotFound>
2025-08-09 15:46:41 -0500

    Finished towers

--- src/main/java/net/mcreator/sovereignhorizon/entity/RivedGhastEntity.java
+++ src/main/java/net/mcreator/sovereignhorizon/entity/RivedGhastEntity.java
@@ -1,11 +1,48 @@
 
 package net.mcreator.sovereignhorizon.entity;
 
-import net.minecraft.nbt.Tag;
-import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.network.PlayMessages;
+import net.minecraftforge.network.NetworkHooks;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.ai.navigation.PathNavigation;
+import net.minecraft.world.entity.ai.navigation.FlyingPathNavigation;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.entity.ai.control.FlyingMoveControl;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.entity.MobType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.Difficulty;
+import net.minecraft.util.RandomSource;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.core.BlockPos;
+
+import net.mcreator.sovereignhorizon.procedures.RivedGhastEntityIsHurtProcedure;
+import net.mcreator.sovereignhorizon.init.SovereignHorizonModEntities;
+
+import java.util.EnumSet;
 
 public class RivedGhastEntity extends Monster {
-
 	public RivedGhastEntity(PlayMessages.SpawnEntity packet, Level world) {
 		this(SovereignHorizonModEntities.RIVED_GHAST.get(), world);
 	}
@@ -15,9 +52,7 @@ public class RivedGhastEntity extends Monster {
 		setMaxUpStep(0f);
 		xpReward = 10;
 		setNoAi(false);
-
 		this.moveControl = new FlyingMoveControl(this, 10, true);
-
 	}
 
 	@Override
@@ -33,7 +68,6 @@ public class RivedGhastEntity extends Monster {
 	@Override
 	protected void registerGoals() {
 		super.registerGoals();
-
 		this.targetSelector.addGoal(1, new NearestAttackableTargetGoal(this, Player.class, false, false));
 		this.goalSelector.addGoal(2, new Goal() {
 			{
@@ -75,7 +109,6 @@ public class RivedGhastEntity extends Monster {
 			}
 		});
 		this.goalSelector.addGoal(3, new RandomStrollGoal(this, 0.8, 20) {
-
 			@Override
 			protected Vec3 getPosition() {
 				RandomSource random = RivedGhastEntity.this.getRandom();
@@ -84,11 +117,9 @@ public class RivedGhastEntity extends Monster {
 				double dir_z = RivedGhastEntity.this.getZ() + ((random.nextFloat() * 2 - 1) * 16);
 				return new Vec3(dir_x, dir_y, dir_z);
 			}
-
 		});
 		this.goalSelector.addGoal(4, new RandomLookAroundGoal(this));
 		this.targetSelector.addGoal(5, new HurtByTargetGoal(this));
-
 	}
 
 	@Override
@@ -113,7 +144,6 @@ public class RivedGhastEntity extends Monster {
 
 	@Override
 	public boolean causeFallDamage(float l, float d, DamageSource source) {
-
 		return false;
 	}
 
@@ -127,7 +157,7 @@ public class RivedGhastEntity extends Monster {
 		Entity sourceentity = damagesource.getEntity();
 		Entity immediatesourceentity = damagesource.getDirectEntity();
 
-		RivedGhastEntityIsHurtProcedure.execute();
+		RivedGhastEntityIsHurtProcedure.execute(entity);
 		if (damagesource.is(DamageTypes.FALL))
 			return false;
 		if (damagesource.is(DamageTypes.CACTUS))
@@ -148,14 +178,12 @@ public class RivedGhastEntity extends Monster {
 
 	public void aiStep() {
 		super.aiStep();
-
 		this.setNoGravity(true);
 	}
 
 	public static void init() {
 		SpawnPlacements.register(SovereignHorizonModEntities.RIVED_GHAST.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
 				(entityType, world, reason, pos, random) -> (world.getDifficulty() != Difficulty.PEACEFUL && Monster.isDarkEnoughToSpawn(world, pos, random) && Mob.checkMobSpawnRules(entityType, world, reason, pos, random)));
-
 	}
 
 	public static AttributeSupplier.Builder createAttributes() {
@@ -165,10 +193,7 @@ public class RivedGhastEntity extends Monster {
 		builder = builder.add(Attributes.ARMOR, 0);
 		builder = builder.add(Attributes.ATTACK_DAMAGE, 3);
 		builder = builder.add(Attributes.FOLLOW_RANGE, 16);
-
 		builder = builder.add(Attributes.FLYING_SPEED, 0.7);
-
 		return builder;
 	}
-
 }
