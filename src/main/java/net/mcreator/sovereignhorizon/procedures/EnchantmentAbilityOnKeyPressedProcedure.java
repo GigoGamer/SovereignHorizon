9f4ebeddd3390e03896380f476a78d832436fe20
GigoGamer <maxprantil@mac.lan>
2025-08-12 12:25:06 -0700

    Fixed bounding, improved entity spawning, made lots of QoL changes.

--- src/main/java/net/mcreator/sovereignhorizon/procedures/EnchantmentAbilityOnKeyPressedProcedure.java
+++ src/main/java/net/mcreator/sovereignhorizon/procedures/EnchantmentAbilityOnKeyPressedProcedure.java
@@ -2,6 +2,10 @@ package net.mcreator.sovereignhorizon.procedures;
 
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.ServerLevel;
@@ -10,19 +14,62 @@ import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.advancements.AdvancementProgress;
 import net.minecraft.advancements.Advancement;
 
+import net.mcreator.sovereignhorizon.init.SovereignHorizonModItems;
+import net.mcreator.sovereignhorizon.init.SovereignHorizonModEnchantments;
+
 public class EnchantmentAbilityOnKeyPressedProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
-		if (world instanceof ServerLevel _level)
-			_level.sendParticles(ParticleTypes.CLOUD, x, y, z, 20, 3, 3, 3, 1);
-		entity.setDeltaMovement(new Vec3(x, (y + 0.5), z));
-		if (entity instanceof ServerPlayer _player) {
-			Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("sovereign_horizon:boing"));
-			AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
-			if (!_ap.isDone()) {
-				for (String criteria : _ap.getRemainingCriteria())
-					_player.getAdvancements().award(_adv, criteria);
+		if (entity.onGround() && (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getEnchantmentLevel(SovereignHorizonModEnchantments.BOUNDING.get()) == 1) {
+			if (world instanceof ServerLevel _level)
+				_level.sendParticles(ParticleTypes.CLOUD, x, y, z, 30, 1.5, 1.5, 1.5, 1);
+			entity.setDeltaMovement(new Vec3((entity.getDeltaMovement().x()), (entity.getDeltaMovement().y() + 1.2), (entity.getDeltaMovement().z())));
+			if (entity instanceof ServerPlayer _player) {
+				Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("sovereign_horizon:boing"));
+				AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+				if (!_ap.isDone()) {
+					for (String criteria : _ap.getRemainingCriteria())
+						_player.getAdvancements().award(_adv, criteria);
+				}
+			}
+		} else if (entity.onGround() && (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getEnchantmentLevel(SovereignHorizonModEnchantments.BOUNDING.get()) == 2) {
+			if (world instanceof ServerLevel _level)
+				_level.sendParticles(ParticleTypes.CLOUD, x, y, z, 30, 1.5, 1.5, 1.5, 1);
+			entity.setDeltaMovement(new Vec3((entity.getDeltaMovement().x()), (entity.getDeltaMovement().y() + 1.75), (entity.getDeltaMovement().z())));
+			if (entity instanceof ServerPlayer _player) {
+				Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("sovereign_horizon:boing"));
+				AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+				if (!_ap.isDone()) {
+					for (String criteria : _ap.getRemainingCriteria())
+						_player.getAdvancements().award(_adv, criteria);
+				}
+			}
+		} else if (entity.onGround() && (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getEnchantmentLevel(SovereignHorizonModEnchantments.BOUNDING.get()) == 3) {
+			if (world instanceof ServerLevel _level)
+				_level.sendParticles(ParticleTypes.CLOUD, x, y, z, 30, 1.5, 1.5, 1.5, 1);
+			entity.setDeltaMovement(new Vec3((entity.getDeltaMovement().x()), (entity.getDeltaMovement().y() + 2), (entity.getDeltaMovement().z())));
+			if (entity instanceof ServerPlayer _player) {
+				Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("sovereign_horizon:boing"));
+				AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+				if (!_ap.isDone()) {
+					for (String criteria : _ap.getRemainingCriteria())
+						_player.getAdvancements().award(_adv, criteria);
+				}
+			}
+		} else if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getEnchantmentLevel(SovereignHorizonModEnchantments.BOUNDING.get()) == 4
+				&& ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getItem() == SovereignHorizonModItems.BLUESKIN_BOOTS_BOOTS.get()
+						|| (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getItem() == Items.LEATHER_BOOTS)) {
+			if (world instanceof ServerLevel _level)
+				_level.sendParticles(ParticleTypes.CLOUD, x, y, z, 30, 1.5, 1.5, 1.5, 1);
+			entity.setDeltaMovement(new Vec3((entity.getDeltaMovement().x()), (entity.getDeltaMovement().y() + 0.8), (entity.getDeltaMovement().z())));
+			if (entity instanceof ServerPlayer _player) {
+				Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("sovereign_horizon:boing"));
+				AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+				if (!_ap.isDone()) {
+					for (String criteria : _ap.getRemainingCriteria())
+						_player.getAdvancements().award(_adv, criteria);
+				}
 			}
 		}
 	}
