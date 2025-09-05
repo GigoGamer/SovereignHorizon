de46cb33f7a6743989d2c67dc6317a9b06549f36
Goshiggy <Shigg@404PageNotFound>
2025-09-04 19:20:09 -0500

    added marble chasms biome, fixed some bugs

--- src/main/java/net/mcreator/sovereignhorizon/procedures/DesolateCreeperPlayerCollidesWithThisEntityProcedure.java
+++ src/main/java/net/mcreator/sovereignhorizon/procedures/DesolateCreeperPlayerCollidesWithThisEntityProcedure.java
@@ -4,25 +4,49 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.GameType;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
+import net.minecraft.client.Minecraft;
 
 public class DesolateCreeperPlayerCollidesWithThisEntityProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
-		if (world instanceof Level _level) {
-			if (!_level.isClientSide()) {
-				_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.creeper.primed")), SoundSource.HOSTILE, 1, 1);
-			} else {
-				_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.creeper.primed")), SoundSource.HOSTILE, 1, 1, false);
+		if (new Object() {
+			public boolean checkGamemode(Entity _ent) {
+				if (_ent instanceof ServerPlayer _serverPlayer) {
+					return _serverPlayer.gameMode.getGameModeForPlayer() == GameType.SURVIVAL;
+				} else if (_ent.level().isClientSide() && _ent instanceof Player _player) {
+					return Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()) != null && Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()).getGameMode() == GameType.SURVIVAL;
+				}
+				return false;
 			}
+		}.checkGamemode(entity) || new Object() {
+			public boolean checkGamemode(Entity _ent) {
+				if (_ent instanceof ServerPlayer _serverPlayer) {
+					return _serverPlayer.gameMode.getGameModeForPlayer() == GameType.ADVENTURE;
+				} else if (_ent.level().isClientSide() && _ent instanceof Player _player) {
+					return Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()) != null && Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()).getGameMode() == GameType.ADVENTURE;
+				}
+				return false;
+			}
+		}.checkGamemode(entity)) {
+			if (world instanceof Level _level) {
+				if (!_level.isClientSide()) {
+					_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.creeper.primed")), SoundSource.HOSTILE, 1, 1);
+				} else {
+					_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.creeper.primed")), SoundSource.HOSTILE, 1, 1, false);
+				}
+			}
+			if (!entity.level().isClientSide())
+				entity.discard();
+			if (world instanceof Level _level && !_level.isClientSide())
+				_level.explode(null, x, y, z, 3, Level.ExplosionInteraction.MOB);
 		}
-		if (!entity.level().isClientSide())
-			entity.discard();
-		if (world instanceof Level _level && !_level.isClientSide())
-			_level.explode(null, x, y, z, 3, Level.ExplosionInteraction.MOB);
 	}
 }
